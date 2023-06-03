c8733013e361e8ed27c3cb8013c7f9f56f4dca4e
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-06-03 09:48:14 +0200

    updated styles and added material icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import{ logo} from '../utils/constants'
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import ModeNightIcon from '@mui/icons-material/ModeNight';
+import SmartDisplayIcon from '@mui/icons-material/SmartDisplay';
+import { red } from '@mui/material/colors'
 const Navbar = () => {
   const [darkMode,setDarkMode]=useState(false)
 
@@ -13,14 +15,19 @@ const Navbar = () => {
     document.body.classList.toggle("dark-mode")
   }
   return (
-    <Stack direction="row" flexDirection="row"  alignItems="center" className={darkMode? "dark-nav": "light-nav"}  p={2} sx={{
+    <Stack direction="row" flexDirection="row"   alignItems="center" className={darkMode? "dark-nav": "light-nav"}  p={2} sx={{
         position:'sticky',
         top:0,
         height:100,
-        justifyContent:"space-between"
+        justifyContent:"space-between",
+        
     }}>
          <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
+      <SmartDisplayIcon sx={{
+        fontSize:40,
+        color:'red'
+      }}/>
+      <Typography variant='h5' sx={{fontWeight:"bold",color:'gray'}}>BRT</Typography>
     </Link>
      <SearchBar/>
      <Box onClick={changeMode}>
