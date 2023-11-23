f0aaae55d1eaf5c7b32af4072a62f92ebe2a10da
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-11-23 13:37:00 +0200

    run build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack } from "@mui/material";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   Feed,
   ChannelDetails,
